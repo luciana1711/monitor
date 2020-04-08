be4de4155e98152cd953f6971be44d2ad33faac4
luciana1711 <lucia.navarro1@gmail.com>
2020-04-08 01:27:23 +0000

    3er commit

--- alimentarthingspeak.sh
+++ alimentarthingspeak.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-KEYWRITE=DQEQRWNY0QASKZBR
+#KEYWRITE=
+. config.cfg
 INSTALL_DIR=${HOME}/monitor
 MEMORIA=$(${INSTALL_DIR}/memoria.sh)
 LOADAVG1=$(${INSTALL_DIR}/loadavg.sh)
 LOADAVG5=$(${INSTALL_DIR}/loadavg.sh 5)
 LOADAVG15=$(${INSTALL_DIR}/loadavg.sh 15)
 DISKUSAGE=$(${INSTALL_DIR}/diskusage.sh)
-curl https://api.thingspeak.com/update?api_key=${KEYWRITE}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}\&field5=${DISKUSAGE}
+curl https://api.thingspeak.com/update?api_key=${WRITEKEY}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}\&field5=${DISKUSAGE}
 
